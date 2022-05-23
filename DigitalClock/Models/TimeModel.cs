7cf768b868ab0b179bcbd82d90a2f47dac76b246
kush530 <47156568+kush530@users.noreply.github.com>
2022-05-23 19:19:09 -0400

    Initial Commit

---
+++ DigitalClock/Models/TimeModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DigitalClock.Models
+{
+    public class TimeModel : INotifyPropertyChanged
+    {
+        private string dateTimeFormat = "T";
+
+        private string dateFormat = "M/d/yy";
+
+        private string dateToDisplay = string.Empty;
+
+        private string dateTimeToDisplay = string.Empty;
+
+        private DateTime currentDateTime;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public TimeModel()
+        {
+            this.UpdateDateTime();
+        }
+
+        public string DateTimeToDisplay
+        {
+            get
+            {
+                return this.dateTimeToDisplay;
+            }
+
+            private set 
+            {
+                this.dateTimeToDisplay = value;
+                this.RaisePropertyChanged("DateTimeToDisplay");
+            }
+        }
+
+        public string DateToDisplay
+        {
+            get
+            {
+                return this.dateToDisplay;
+            }
+
+            private set
+            {
+                this.dateToDisplay = value;
+                this.RaisePropertyChanged("DateToDisplay");
+            }
+        }
+
+        public void UpdateDateTime()
+        {
+            this.currentDateTime = DateTime.Now;
+
+            this.DateToDisplay = this.currentDateTime.ToString(this.dateFormat);
+            this.DateTimeToDisplay = this.currentDateTime.ToString(this.dateTimeFormat);
+        }
+
+        private void RaisePropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if(handler == null)
+            {
+                return;
+            }
+
+            handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DigitalClock/ViewModels/TimeViewModel.cs
@@ -0,0 +1,40 @@
+﻿using DigitalClock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace DigitalClock.ViewModels
+{
+    public class TimeViewModel
+    {
+        private Timer timer;
+
+        private TimeModel timeModel;
+
+        public TimeModel Time
+        {
+            get { return this.timeModel; }
+        }
+
+        public TimeViewModel()
+        {
+            this.timeModel = new TimeModel();
+
+            this.timer = new Timer();
+
+            this.timer.Interval = 1000;
+
+            this.timer.Elapsed += this.onTimerElapsed;
+
+            this.timer.Start();
+        }
+
+        private void onTimerElapsed(object sender, ElapsedEventArgs eventArgs)
+        {
+            this.timeModel.UpdateDateTime();
+        }
+    }
+}
